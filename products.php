c26891cd236c8db5f197136ddcd1e62001315d08
Stefan <moc@semosedu.local>
2017-05-11 13:56:27 +0200

    update

--- products.php
+++ products.php
@@ -24,49 +24,18 @@
 		<div class="categories-holder">
 			<div class="categories">
 				<ul class="produkti">
-<?php
-	foreach($products as $key => $pro) {
-		echo '<li>'.$pro['ime'];'</li>';
-	} 
-?>
-
-					<!-- <li><a href="#" class="h1">Бои и Лакови</a></li> -->
-<!-- 				<li><a href="#"></a></li>  // ZA PODKATEGORII
-					<li><a href="#"></a></li> -->
-
-					<!-- <li><a href="#" class="h1">Железарија</a></li> -->
-<!-- 					<li><a href="#"></a></li>		
-					<li><a href="#"></a></li> -->
-
-					<!-- <li><a href="#" class="h1">Електрика</a></li>
-					<li><a href="#"></a></li>	
-					<li><a href="#"></a></li>
-					<li><a href="#"></a></li> -->
-
-					<!-- <li><a href="#" class="h1">Водовод</a></li> -->
-<!-- 					<li><a href="#"></a></li>
-					<li><a href="#"></a></li>
-					<li><a href="#"></a></li> -->
-
-					<!-- <li><a href="#" class="h1">Санитарија</a></li> -->
-<!-- 					<li><a href="#"></a></li>
-					<li><a href="#"></a></li>
-					<li><a href="#"></a></li> -->
-
-					<!-- <li><a href="#" class="h1">Градежни Материјали</a></li> -->
-<!-- 					<li><a href="#"></a></li>
-					<li><a href="#"></a></li>
-					<li><a href="#"></a></li> -->
+				<?php
+					foreach($products as $key => $pro) {
+						echo '<li>'.$pro['ime'];'</li>';
+					} 
+				?>
 				</ul>
 			</div>
 		</div>
 		<div class="products-holder">
 			<div class="products">
-				<ul>
-					<li><div class="product"><a href="#"></a></div></li>
-					<li><div class="product"><a href="#"></a></div></li>
-					<li><div class="product"><a href="#"></a></div></li>
-				</ul>
+		
+
 			</div>
 		</div>
 	</div>
